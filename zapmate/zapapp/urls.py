fd7c4887771762151f544b7a339d9641c5323f5e
Rushil246 <102139649+Rushil246@users.noreply.github.com>
2023-08-27 22:09:36 +0530

    profile 2

--- zapmate/zapapp/urls.py
+++ zapmate/zapapp/urls.py
@@ -5,6 +5,7 @@ urlpatterns = [
     path('users/<int:pk>/', UserRetrieveUpdateDestroyView.as_view(), name='user-retrieve-update-destroy'),
     path('register/', UserRegistrationView.as_view(), name='user-registration'),
     path('api/token/', UserLoginView.as_view(), name='token_obtain_pair'),
+    path('profiles/', ProfileListCreateView.as_view(), name='profile-list-create'),
 
     
 
