fa088c6508ef4c3a56329441f7caa63c3346cabe
SAHIL KUMAR SINGH <s1610.2003@gmail.com>
2023-08-27 23:09:43 +0530

    time increase token

--- zapmate/zapmate/settings.py
+++ zapmate/zapmate/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-
+import datetime
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -94,6 +94,8 @@ DATABASES = {
 }
 AUTH_USER_MODEL = 'zapapp.CustomUser'
 
+
+JWT_EXPIRATION_DELTA = datetime.timedelta(days=1)
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
 
